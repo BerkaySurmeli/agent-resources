def528ca32b5d29e386eb65ca9b27d729e0757c5
Berkay Surmeli <berkay@example.com>
2026-04-14 09:30:36 -0700

    fix: Add AuthProvider and CartProvider to _app.tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { LanguageProvider } from '../context/LanguageContext';
+import { AuthProvider } from '../context/AuthContext';
+import { CartProvider } from '../context/CartContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <LanguageProvider>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <CartProvider>
+          <Component {...pageProps} />
+        </CartProvider>
+      </AuthProvider>
     </LanguageProvider>
   );
 }
